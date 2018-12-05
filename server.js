674d59720c0487a81a2d04bb8bacf509907c90d0
JulieLy8619 <july8619@yahoo.com>
2018-12-05 12:57:42 -0800

    jl simplify version

--- server.js
+++ server.js
@@ -3,42 +3,19 @@
 // app dependencies
 const express = require('express');
 const superagent = require('superagent');
-const pg = require('pg');
-const methodOverride = require('method-override');
 
 // load environment variables from .env files
-require ('dotenv').config();
+// require ('dotenv').config();
 
 // app setup
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
-// application middleware
-app.use(express.urlencoded({extended:true}));
+
 app.use(express.static('public'));
+app.use(express.static('public/styles'))
 
-//middleware to handle PUT and DELETE
-app.use(methodOverride((request, response) => {
-  if (request.body && typeof request.body === 'object' && '_method' in request.body) {
-    // look in urlencoded POST bodies and delete it
-    let method = request.body._method;
-    delete request.body._method;
-    return method;
-  }
-}))
-
-// set view
-// app.set('view engine', 'ejs')
-
-//database setup
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
-client.on('error', err => console.error(err));
-
-function handleError(err, res) {
-  console.error(err);
-  res.render('pages/error', {error: err});
-}
+app.set('view engine', 'ejs')
 
 //view routes
 //just the test for proof of life
@@ -46,15 +23,7 @@ app.get('/', (request, response) => {
   response.render('pages/index');
 })
 
-//api routes
-
-//catchall
 app.get('*', (request, response) => response.status(404).send('This route does not exist.'));
 
 // listening
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-//Helper Functions
-
-
-//constructors/models
